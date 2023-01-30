9408e77c90fdc93491f44bd618befff61dcf2494
Literally Void <literallyvoid@gmail.com>
2023-01-29 21:41:37 -0800

    initial commit

---
+++ src/opt-codegen.c
@@ -0,0 +1,126 @@
+static char *start;
+static char *code;
+
+// Registers:
+// BL: current value of cell
+// RBP: pointer to current cell
+
+void writeU8(uint8_t i) {
+	code[0] = i;
+	code++;
+}
+
+void writeU16(uint16_t i) {
+	writeU8(i);
+	writeU8(i >> 8);
+}
+
+void writeU32(uint32_t i) {
+	writeU16(i);
+	writeU16(i >> 16);
+}
+
+static void writeOpDigitReg(uint8_t op, int digit, int reg) {
+	writeU8(op);
+	writeU8(0xC0 | (digit << 3) | (reg << 0));
+}
+
+static void writeInc(void) {
+	// INC: FE /0 reg/mem8
+	writeOpDigitReg(0xFE, 0, 3);
+}
+
+static void writeDec(void) {
+	// DEC: FE /1 reg/mem8
+	writeOpDigitReg(0xFE, 1, 3);
+}
+
+static void writeMovBlRbp(uint8_t opcode) {
+	int reg = 3; // BL
+	int regmem = 5; // RBP
+
+	//writeU8(0x40); // Empty REX to allow RSP/RBP/RSI/RDI addressing
+	writeU8(opcode);
+	writeU8(0x80 | (reg << 3) | (regmem << 0));
+	writeU32(0);
+}
+
+static void writeStore(void) {
+	// AL -> BYTE PTR [RBP]
+	writeMovBlRbp(0x88);
+}
+
+static void writeLoad(void) {
+	// BYTE PTR [RBP] -> AL
+	writeMovBlRbp(0x8A);
+}
+
+static void writeRbpMod(uint8_t opcode, uint8_t digit) {
+	int reg = 5; // RBP
+
+	writeU8(0x40 | (8)); // REX .X
+	writeU8(opcode);
+	writeU8(0xC0 | (digit << 3) | (reg << 0));
+}
+
+static void writeMovL(void) {
+	writeStore();
+	writeRbpMod(0xFF, 1);
+	writeLoad();
+}
+
+static void writeMovR(void) {
+	writeStore();
+	writeRbpMod(0xFF, 0);
+	writeLoad();
+}
+
+static void writeMovImm(uint8_t reg, uint32_t value) {
+	int rex_r = (reg & 0x8) ? 4 : 0;
+	writeU8(0x40 | rex_r);
+	writeU8(0xB8 + (reg & 0x7));
+	writeU32(value);
+}
+
+static void writeSyscall(uint32_t no) {
+	writeMovImm(0b000, no); // RAX = no
+	// syscall: 0F 05
+	writeU8(0x0F);
+	writeU8(0x05);
+}
+
+static void writeSyscallImm(uint32_t no, uint32_t a, uint32_t b, uint32_t c) {
+	writeMovImm(0b111, a); // RDI = a
+	writeMovImm(0b110, b); // RSI = b
+	writeMovImm(0b010, c); // RDX = c
+
+	writeSyscall(no);
+}
+
+static void writeEmit(void) {
+	writeStore();
+
+	// syscall arg0: fd
+	writeMovImm(0b111, 0 /* stdout */);
+
+	// syscall arg1: ptr
+	// MOV RSI, RBP
+	writeU8(0x40 | 8); // REX.W
+	writeOpDigitReg(0x8B, 0b110, 0b101);
+
+	// syscall arg2: count (1)
+	writeMovImm(0b010, 1);
+
+	writeSyscall(1);
+}
+
+static void writeRead(void) {
+	writeU8(0x90);
+}
+
+static void patchU32(char *code_ptr, uint32_t value) {
+	char *code_sv = code;
+	code = code_ptr;
+	writeU32(value);
+	code = code_sv;
+}
---
+++ src/opt.c
@@ -0,0 +1,236 @@
+#include <stdio.h>
+#include <sys/mman.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+enum OpKind {
+	OP_NOP = 0,
+	OP_END,
+
+	OP_ADD_PTR,
+
+	OP_ADD,
+
+	OP_LABEL,
+	OP_JMP,
+	OP_BR_IFNZ,
+
+	OP_READ,
+	OP_WRITE,
+};
+
+struct OpCode {
+	enum OpKind kind;
+	union {
+		uint8_t add; // OP_ADD
+		int32_t mem_offset; // OP_LOAD, OP_STORE, OP_ADD_PTR
+		uint32_t label; // OP_LABEL, OP_JMP, OP_BR_IFZ
+		void *jump; // OP_JMP, OP_BR_IFNZ
+	};
+};
+
+#define MAX_CODE_LEN 8 * 1024 * 1024
+
+static struct OpCode *ops, *ops_into;
+
+void swap(void) {
+	struct OpCode *tmp = ops;
+	ops = ops_into;
+	ops_into = tmp;
+}
+
+void dumpOps(void) {
+	for (size_t i = 0; ops[i].kind != OP_END; i++) {
+		switch (ops[i].kind) {
+		case OP_NOP: printf("\tNOP\n"); break;
+		case OP_END: printf("\tEND\n"); break;
+		case OP_ADD_PTR: printf("\tADD_PTR %d\n", ops[i].mem_offset); break;
+		case OP_ADD: printf("\tADD %d\n", (int) ops[i].add); break;
+		case OP_LABEL: printf("%u:\n", ops[i].label); break;
+		case OP_JMP: printf("\tJMP %u\n", ops[i].label); break;
+		case OP_BR_IFNZ: printf("\tJNZ %u\n", ops[i].label); break;
+		case OP_READ: printf("\tREAD\n"); break;
+		case OP_WRITE: printf("\tWRITE\n"); break;
+		}
+	}
+}
+
+void coalesce(void) {
+	size_t i;
+	size_t last = 0;
+	for (i = 0; ops[i].kind != OP_END; i++) {
+		ops_into[last] = ops[i];
+
+		if (last > 0 && ops_into[last - 1].kind == ops[i].kind) {
+			if (ops[i].kind == OP_ADD) {
+				ops_into[last - 1].add += ops[i].add;
+				continue;
+			}
+
+			if (ops[i].kind == OP_ADD_PTR) {
+				ops_into[last - 1].mem_offset += ops[i].mem_offset;
+				continue;
+			}
+		}
+
+		last++;
+	}
+
+	ops_into[last] = ops[i];
+}
+
+void runCodegen(void) {
+	printf(".intel_syntax noprefix\n");
+	printf(".globl _start\n");
+	printf("_start:\n");
+	printf("\tmov RAX, 0x4000\n");
+	printf("loop:\n");
+	printf("\tsub RSP, 4\n");
+	printf("\tmov DWORD PTR [RSP], 0\n");
+	printf("\tsub RAX, 1\n");
+	printf("\tjnz loop\n");
+	printf("\tmov RBP, RSP\n");
+
+	for (size_t i = 0; ops[i].kind != OP_END; i++) {
+		switch (ops[i].kind) {
+		case OP_NOP:
+			printf("\tnop\n");
+			break;
+		case OP_END:
+			break;
+		case OP_ADD_PTR:
+			printf("\tmov BYTE PTR [RBP], BL\n");
+			printf("\tadd RBP, %d\n", ops[i].mem_offset);
+			printf("\tmov BL, BYTE PTR [RBP]\n");
+			break;
+		case OP_ADD:
+			printf("\tadd BL, %u\n", ops[i].add);
+			break;
+		case OP_LABEL:
+			printf("l%u:\n", ops[i].label);
+			break;
+		case OP_JMP:
+			printf("\tjmp l%u\n", ops[i].label);
+			break;
+		case OP_BR_IFNZ:
+			printf("\tcmp BL, 0\n");
+			printf("\tjnz l%u\n", ops[i].label);
+			break;
+		case OP_READ:
+			printf("\t// TODO: PANIC!\n");
+			break;
+		case OP_WRITE:
+			printf("\tmov RAX, 1\n");
+			printf("\tmov RDI, 0\n");
+			printf("\tmov RSI, RBP\n");
+			printf("\tmov RDX, 1\n");
+			printf("\tsyscall\n");
+			break;
+		}
+	}
+
+	printf("\tmov RAX, 60\n");
+	printf("\tmov RDI, 42\n");
+	printf("\tsyscall\n");
+}
+
+int main(int argc, char **argv) {
+	ops = malloc(sizeof(struct OpCode) * MAX_CODE_LEN);
+	ops_into = malloc(sizeof(struct OpCode) * MAX_CODE_LEN);
+
+	char *source = NULL;
+
+	FILE *sfp = fopen(argv[1], "rb");
+	if (!sfp) return 3;
+
+	source = malloc(8 * 1024 * 1024);
+
+	ssize_t amt = fread(source, 1, 8 * 1024 * 1024, sfp);
+	if (amt < 0) return 9;
+	source[amt] = 0;
+
+	fclose(sfp);
+
+	uint32_t stack[64];
+	uint32_t stack_i = 0;
+
+	uint32_t last_label = 0;
+
+	int last = 0;
+	for (; *source; source++) {
+		switch (*source) {
+		case '+':
+			ops_into[last++] = (struct OpCode) {
+				.kind = OP_ADD,
+				.add = 1,
+			};
+			break;
+		case '-':
+			ops_into[last++] = (struct OpCode) {
+				.kind = OP_ADD,
+				.add = 255,
+			};
+			break;
+		case '<':
+			ops_into[last++] = (struct OpCode) {
+				.kind = OP_ADD_PTR,
+				.mem_offset = -1,
+			};
+			break;
+		case '>':
+			ops_into[last++] = (struct OpCode) {
+				.kind = OP_ADD_PTR,
+				.mem_offset = 1,
+			};
+			break;
+		case '[':
+			stack[stack_i++] = last;
+			ops_into[last++] = (struct OpCode) {
+				.kind = OP_JMP,
+				.label = last_label,
+			};
+			ops_into[last++] = (struct OpCode) {
+				.kind = OP_LABEL,
+				.label = last_label,
+			};
+			last_label++;
+			break;
+		case ']': {
+			uint32_t jump_tgt = stack[--stack_i];
+			ops_into[jump_tgt].label = last_label;
+			ops_into[last++] = (struct OpCode) {
+				.kind = OP_LABEL,
+				.label = last_label,
+			};
+			ops_into[last++] = (struct OpCode) {
+				.kind = OP_BR_IFNZ,
+				.label = ops_into[jump_tgt + 1].label,
+			};
+			last_label++;
+		} break;
+		case '.':
+			ops_into[last++] = (struct OpCode) {
+				.kind = OP_WRITE,
+			};
+			break;
+		case ',':
+			ops_into[last++] = (struct OpCode) {
+				.kind = OP_READ,
+			};
+			break;
+		}
+	}
+
+	ops_into[last++] = (struct OpCode) {
+		.kind = OP_END,
+		.add = 1,
+	};
+
+	swap();
+
+	coalesce();
+	swap();
+
+	runCodegen();
+}
